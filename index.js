0867b05770eaa8779310eaa849f284c50293d302
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-11-06 07:29:17 +0600

    create orderColletion and order API to recive order data

--- index.js
+++ index.js
@@ -16,18 +16,30 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('geniusCar').collection('services');
+        const orderCollection = client.db('geniusCar').collection('orders');
+        
+        
         app.get('/services', async(req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
 
-            app.get('/services/:id', async(req, res) => {
-                const id = req.params.id;
-                const query = {_id: ObjectId(id)};
-                const service = await serviceCollection.findOne(query);
-                res.send(service);
-            })
+
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+
+        // order API
+        app.post('/orders', async(req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         })
     }
     finally {
