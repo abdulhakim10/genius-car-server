d9f23e1478561935e1fbb1290dfaa2b1d8ad5dfd
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-11-07 22:33:43 +0600

    create jwt token API by using post

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -17,7 +18,13 @@ async function run() {
     try {
         const serviceCollection = client.db('geniusCar').collection('services');
         const orderCollection = client.db('geniusCar').collection('orders');
+  
         
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+            res.send({token})
+        })
 
         app.get('/services', async(req, res) => {
             const query = {};
